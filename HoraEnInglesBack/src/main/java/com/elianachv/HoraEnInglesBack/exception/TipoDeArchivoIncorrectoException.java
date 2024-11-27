963b3942541c8904f38b96e085ad649a674c1709
Eliana Chavez Valencia <elianachavezv@gmail.com>
2024-11-27 15:08:04 -0500

    :sparkles: PT001: Se crea aplicativo backend

---
+++ HoraEnInglesBack/src/main/java/com/elianachv/HoraEnInglesBack/exception/TipoDeArchivoIncorrectoException.java
@@ -0,0 +1,10 @@
+package com.elianachv.HoraEnInglesBack.exception;
+
+public class TipoDeArchivoIncorrectoException extends Exception {
+    public TipoDeArchivoIncorrectoException() {
+    }
+
+    public TipoDeArchivoIncorrectoException(String message) {
+        super(message);
+    }
+}
